b5d9b61ae945e9e19595f1beba92236bc166bf30
weepper <pene.remi@gmail.com>
2021-04-11 12:21:18 +0200

    flow design

--- gui/main.py
+++ gui/main.py
@@ -1,30 +1,57 @@
 import sys
 import json
-from PyQt5.QtWidgets import QMainWindow, QMenu
+from PyQt5.QtWidgets import QMainWindow, QMenu, QLabel, QPushButton, QWidget, QScrollArea, QVBoxLayout, QHBoxLayout
 
 from gui.param import paramWindow
+from gui.widgets import FlowLayout
 #from app import setting
 
 class mainWindow(QMainWindow):
     def __init__(self, app):
-        super().__init__()
+        super(mainWindow, self).__init__()
 
         self.paramW = None
         self.screen = [app.primaryScreen().size().width(), app.primaryScreen().size().height()]
-        #self.setStyleSheet("background-color: #33343b; color: #AAAAAA")
         self.setWindowTitle("PyPop")
-        self.resize(int(self.screen[0] * 0.7), int(self.screen[1] * 0.7))
 
         self._createMenuBar()
+
+        self.mainLayout = QVBoxLayout()
+
+        self.grid = FlowLayout()
+        self.grid.addWidget(QPushButton("Short"))
+        self.grid.addWidget(QPushButton("Longer"))
+        self.grid.addWidget(QPushButton("Different text"))
+        self.grid.addWidget(QPushButton("More text"))
+        self.grid.addWidget(QPushButton("Even longer button text"))
+        self.grid.addWidget(QPushButton("Short"))
+        self.grid.addWidget(QPushButton("Longer"))
+        self.grid.addWidget(QPushButton("Different text"))
+        self.grid.addWidget(QPushButton("More text"))
+        self.grid.addWidget(QPushButton("Even longer button text"))
+        self.grid.addWidget(QPushButton("Short"))
+        self.grid.addWidget(QPushButton("Longer"))
+        self.grid.addWidget(QPushButton("Different text"))
+        self.grid.addWidget(QPushButton("More text"))
+        self.grid.addWidget(QPushButton("Even longer button text"))
+
+        self.scroll = QScrollArea(self)
+        self.scroll.setWidget(self.grid.asWidget())
+        self.scroll.setWidgetResizable(True)
+        #self.scroll.setFixedHeight(400)
+
+        self.setCentralWidget(self.scroll)
+        #print(self.grid.children())
     
     def _createMenuBar(self):
-        menuBar = self.menuBar()
+        self.menu = self.menuBar()
         optionMenu = QMenu("&Option", self)
         optionMenu.addAction("parameter", self.openParameter)
         optionMenu.addAction("quit", self.close)
-        menuBar.addMenu(optionMenu)
+        self.menu.addMenu(optionMenu)
 
     def openParameter(self):
+        self.grid.addWidget(QPushButton("nice"))
         if self.paramW is None:
             self.paramW = paramWindow(self.screen)
         self.paramW.show()
--- gui/widgets.py
+++ gui/widgets.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QSlider, QLabel, QHBoxLayout, QLineEdit
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QSlider, QLabel, QSizePolicy, QHBoxLayout, QLineEdit, QLayout, QWidget
+from PyQt5.QtCore import QPoint, QRect, QSize, Qt
 
 class sliderInd(QHBoxLayout):
     def __init__(self, min = 0, max = 100, value = 0, path = '', alias = ''):
@@ -7,7 +7,6 @@ class sliderInd(QHBoxLayout):
 
         #display alias
         self.ali = QLabel(str(alias))
-        #self.ali = QLabel("<font color=#33343b>" + str(alias) + "</font>")
         self.ali.setMinimumWidth(200)
 
         #display slider
@@ -18,7 +17,7 @@ class sliderInd(QHBoxLayout):
         self.inp.setObjectName(path)
 
         #display value
-        self.ind = QLabel("<font color=#33343b>" + str(value) + "</font>")
+        self.ind = QLabel(str(value))
         self.ind.setObjectName(path + 'Ind')
         self.addWidget(self.ali)
         self.addWidget(self.inp)
@@ -28,14 +27,14 @@ class sliderInd(QHBoxLayout):
         self.inp.valueChanged.connect(self.valueChanged)
     
     def valueChanged(self):
-        self.ind.setText("<font color=#33343b>" + str(self.inp.value()) + "</font>")
+        self.ind.setText(str(self.inp.value()))
 
 class textInput(QHBoxLayout):
     def __init__(self, value = 0, path = '', alias = ''):
         super().__init__()
 
         #display alias
-        self.ali = QLabel("<font color=#33343b>" + str(alias) + "</font>")
+        self.ali = QLabel(str(alias))
         self.ali.setMinimumWidth(200)
 
         #display slider
@@ -45,3 +44,94 @@ class textInput(QHBoxLayout):
 
         self.addWidget(self.ali)
         self.addWidget(self.inp)
+
+class FlowLayout(QLayout):
+    def __init__(self, parent=None, margin=0, spacing=-1):
+        super(FlowLayout, self).__init__(parent)
+
+        if parent is not None:
+            self.setContentsMargins(margin, margin, margin, margin)
+
+        self.setSpacing(spacing)
+
+        self.itemList = []
+
+    def __del__(self):
+        item = self.takeAt(0)
+        while item:
+            item = self.takeAt(0)
+
+    def addItem(self, item):
+        self.itemList.append(item)
+
+    def count(self):
+        return len(self.itemList)
+
+    def itemAt(self, index):
+        if index >= 0 and index < len(self.itemList):
+            return self.itemList[index]
+
+        return None
+
+    def takeAt(self, index):
+        if index >= 0 and index < len(self.itemList):
+            return self.itemList.pop(index)
+
+        return None
+
+    def expandingDirections(self):
+        return Qt.Orientations(Qt.Orientation(0))
+
+    def hasHeightForWidth(self):
+        return True
+
+    def heightForWidth(self, width):
+        height = self.doLayout(QRect(0, 0, width, 0), True)
+        return height
+
+    def setGeometry(self, rect):
+        super(FlowLayout, self).setGeometry(rect)
+        self.doLayout(rect, False)
+
+    def sizeHint(self):
+        return self.minimumSize()
+
+    def minimumSize(self):
+        size = QSize()
+
+        for item in self.itemList:
+            size = size.expandedTo(item.minimumSize())
+
+        margin, _, _, _ = self.getContentsMargins()
+
+        size += QSize(2 * margin, 2 * margin)
+        return size
+
+    def doLayout(self, rect, testOnly):
+        x = rect.x()
+        y = rect.y()
+        lineHeight = 0
+
+        for item in self.itemList:
+            wid = item.widget()
+            spaceX = self.spacing() + wid.style().layoutSpacing(QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Horizontal)
+            spaceY = self.spacing() + wid.style().layoutSpacing(QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Vertical)
+            nextX = x + item.sizeHint().width() + spaceX
+            if nextX - spaceX > rect.right() and lineHeight > 0:
+                x = rect.x()
+                y = y + lineHeight + spaceY
+                nextX = x + item.sizeHint().width() + spaceX
+                lineHeight = 0
+
+            if not testOnly:
+                item.setGeometry(QRect(QPoint(x, y), item.sizeHint()))
+
+            x = nextX
+            lineHeight = max(lineHeight, item.sizeHint().height())
+
+        return y + lineHeight - rect.y()
+
+    def asWidget(self):
+        widget = QWidget()
+        widget.setLayout(self)
+        return widget
